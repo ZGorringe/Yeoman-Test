b812c252cee4d853a659173f9a94f2fbdaeb18eb
ZGorringe <zacgorringe@gmail.com>
2014-10-28 12:19:19 -0600

    Have initial framework and initial site stylings finished

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientApp
+ */
+angular.module('CFBTweet')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
